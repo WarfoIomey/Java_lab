b999cfa52266f8e80c3ee61f768f13ac6d4e6ccc
Kirill Turochkin <kirill180104@mail.ru>
2024-11-29 15:12:26 +0800

    Labrotory 4

---
+++ lab4/task02.java
@@ -0,0 +1,17 @@
+package lab4;
+
+public class task02 {
+    public static void main(String[] args) {
+        int str = 11;
+        int length;
+        int i;
+        int z;
+
+        for (i=1; i <= str; i++) {
+            for (z=0; i > z; z++) {
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+    }
+}
